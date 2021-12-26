93059f94fabfb2339d5f2837442c1fb267b1555f
Kapeet <itamar246@gmail.com>
2021-12-26 13:26:56 +0200

    Fixed Infinite scroller, added seamless background

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+import Divider from '@mui/material/Divider'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
+
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 type CommentProps = {
     onClickedNewCommentButton: Function
 }
@@ -16,22 +20,44 @@ const style = {
         margin: '1em auto'
     }
 }
-const Comments: React.FunctionComponent<CommentProps> = ({onClickedNewCommentButton}) => {
-    const [APIcomments, setAPIcomments] = useState<Array<any>>([]);
+const amountOfCommentsToShow: number = 10;
+const Comments: React.FunctionComponent<CommentProps> = ({ onClickedNewCommentButton }) => {
+    const [APIcomments, setAPIcomments] = useState<Array<Object>>([]);
     const [userScrolls, setUserScrolls] = useState<number>(0); //check if the user scrolled down, this is used to load more or less comments.
 
+    const fetchData = () => {
+        fetch(`http://jsonplaceholder.typicode.com/comments?_start=${userScrolls}&_end=${userScrolls + amountOfCommentsToShow}`)
+            .then(response => response.json())
+            .then(data => {
+                if (APIcomments && APIcomments.length) {
+                    let combinedComments: Array<Object> = APIcomments.concat(data);
+                    console.log(combinedComments);
+                    console.log(APIcomments);
+                    setAPIcomments(combinedComments);
+                }
+                else {
+                    setAPIcomments(data);
+                }
+            });
+    };
     useEffect(() => {
-        fetch(`http://jsonplaceholder.typicode.com/comments?_start=${userScrolls}&_end=${userScrolls + 5}`)
-        .then(response => response.json())
-        .then(data => {
-            setAPIcomments(data);
-        });
-    },[])
+        fetchData();
+    }, [userScrolls])
     return (
         <div>
-            <h1>Comments component lolol</h1>
             <Button color="secondary" variant="contained" onClick={() => onClickedNewCommentButton()}>Add Comment</Button>
-            {APIcomments ? <CommentList comments={APIcomments} /> : <h1>No comments yet..</h1>}
+            {APIcomments ?
+                <InfiniteScroll
+                    dataLength={APIcomments ? APIcomments.length : amountOfCommentsToShow}
+                    next={() => setUserScrolls(userScrolls + amountOfCommentsToShow)}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                >
+                    <CommentList
+                        comments={APIcomments}
+                    />
+                </InfiniteScroll>
+                : <h1>No comments yet..</h1>}
         </div>
     )
 };
@@ -39,36 +65,24 @@ const Comments: React.FunctionComponent<CommentProps> = ({onClickedNewCommentBut
 
 type CommentListProps = {
     comments: Array<any>
+    // userScrolls: number
+    // setUserScrolls: Function
+    // fetchData: Function,
 }
-const CommentList: React.FunctionComponent<CommentListProps> = ({comments}) => {
+const CommentList: React.FunctionComponent<CommentListProps> = ({ comments }) => {
     return (
-        <List sx={{ width: '100%', maxWidth: 360, gap: 2,m: '0 auto'}}>
+        <List sx={{ width: '100%', maxWidth: 360, gap: 2, m: '0 auto' }}>
             {comments.map(comment => {
                 return (
-                    <Card style={style.card}
-                          sx={{ minWidth: 275 }} key={comment.name}>
+                    <Card style={style.card} key={comment.name}
+                        sx={{ minWidth: 275 }} >
                         <CardContent>
                             <ListItem alignItems="flex-start">
                                 <ListItemText
                                     primary={comment.name}
-                                    secondary={
-                                        <React.Fragment>
-                                        <Typography
-                                        sx={{ display: 'inline' }}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                        >
-                                        {comment.email}
-                                        </Typography>
-                                        <Divider />
-                                        <Typography>
-                                        {comment.body}
-
-                                        </Typography>
-                                    </React.Fragment>
-                                    }
-                                    />
+                                    disableTypography
+                                    secondary={<CommentBody email={comment.email} body={comment.body} />}
+                                />
                             </ListItem>
                         </CardContent>
                     </Card>
@@ -77,4 +91,26 @@ const CommentList: React.FunctionComponent<CommentListProps> = ({comments}) => {
         </List>
     )
 };
+
+type CommentBodyProps = {
+    email: string,
+    body: string,
+}
+const CommentBody: React.FunctionComponent<CommentBodyProps> = ({ email, body }) => {
+
+    return (
+        <div>
+            <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+            >
+                {email}
+            </Typography>
+            <Divider />
+            <Typography component="span">{body}</Typography>
+        </div>
+    )
+}
 export default Comments;
--- src/components/newCommentForm.tsx
+++ src/components/newCommentForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ImCancelCircle} from 'react-icons/im';
+import { ImCancelCircle } from 'react-icons/im';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -21,7 +21,6 @@ const style = {
         transform: 'translate(-50%, -50%)',
         width: "25em",
         bgcolor: 'background.paper',
-        border: '2px solid #000',
         boxShadow: 26,
         p: 3,
     },
@@ -42,37 +41,22 @@ const style = {
     }
 };
 
-const NewCommentForm = ({isFormActive, setFormActive, handleSubmit}: newCommentProps) => {
+const NewCommentForm = ({ isFormActive, setFormActive, handleSubmit }: newCommentProps) => {
     return (
-        // <div className="newCommentForm">
-        //     <form onSubmit={(e) => handleSubmit(e)}>
-        //         <label>
-        //             help
-        //             <input type="text" />
-        //         </label>
-        //         <button type="submit">Submit new Comment</button>
-        //     </form>
-        // </div>
         <Modal
-        open={isFormActive}
-        onClose={() => handleSubmit()}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+            open={isFormActive}
+            onClose={() => handleSubmit()}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
         >
             <Box sx={style.box}>
-                {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Please fill this form.
-            </Typography> */}
-                <div style={{display: 'flex'}}>
+                <div style={{ display: 'flex' }}>
                     <TextField style={style.input} id="fullname" label="Your Full Name" variant="standard" />
                     <TextField style={style.input} id="email" label="Your Email" variant="standard" />
                 </div>
                 <TextField style={style.comment} id="commentbody" label="Your Comment" multiline variant="outlined" />
                 <div style={style.flexDiv}>
                     <Button style={style.Btn} variant="contained">Add new comment</Button>
-                    {/* <IconButton color="primary" aria-label="Cancel"  onClick={() => setFormActive(false)} >
-                        <CancelIcon />
-                    </IconButton> */}
 
                     <Button
                         color="error"
